bc5b66e505f5ed700bf367e1d3f52bbbff0a9c00
Shcoobz <a.boeckmann23@gmail.com>
2024-05-12 19:26:31 +0200

    implemented app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import PlayerControls from './components/PlayerControls';
+import ProgressBar from './components/ProgressBar';
+import SongInfo from './components/SongInfo';
+import songs from './songs';
 
 function App() {
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function handlePlayPause() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function handlePrevSong() {
+    setCurrentSongIndex((prevIndex) =>
+      prevIndex === 0 ? songs.length - 1 : prevIndex - 1
+    );
+  }
+
+  function handleNextSong() {
+    setCurrentSongIndex((prevIndex) =>
+      prevIndex === songs.length - 1 ? 0 : prevIndex + 1
+    );
+  }
+
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className='player-container'>
+      <SongInfo song={songs[currentSongIndex]} />
+      <ProgressBar isPlaying={isPlaying} song={songs[currentSongIndex]} />
+      <PlayerControls
+        isPlaying={isPlaying}
+        onPlayPause={handlePlayPause}
+        onPrevSong={handlePrevSong}
+        onNextSong={handleNextSong}
+      />
     </div>
   );
 }
